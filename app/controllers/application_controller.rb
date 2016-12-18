3863a52822f93e1912eab1f08e2ba74f901a90e9
Dipen Chauhan <get.dipen@gmail.com>
2016-12-18 12:06:41 -0800

    Overriding the current_user Method in Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+
+	# Override the default method
+	def authenticate_user!
+		current_user.present?
+	end
+
+	# Override the default method
+	# define our unregistered visitors, or Soft Users
+
+	def current_user
+		super || User.where(soft_token: soft_token).first_or_initialize
+	end
+
+	private
+
+	def soft_token
+	  session[:user_token] ||= SecureRandom.hex(8)
+	end
 end

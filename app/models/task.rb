0b4f5d526f1ade48b6f37329bc5af51215ee3bd2
Dipen Chauhan <get.dipen@gmail.com>
2016-12-18 19:37:58 -0800

    Added complete & incomplete scopes and defined mark_complete!

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
-  belongs_to :user
+	belongs_to :user
+	scope :complete, -> { where(complete: true) }
+	scope :incomplete, -> { where(complete: nil) }
+
+	def mark_complete!
+		self.update_attribute(:complete, true)
+	end
 end
